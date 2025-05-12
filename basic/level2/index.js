16c5c2415e57b6f80bb25e02c7b9691676434ab6
Abhishek Gaire <abhisekgaire7@gmail.com>
2025-05-12 22:18:49 +0545

    fix: cors added in task2 for task2

--- basic/level2/index.js
+++ basic/level2/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
 const port = 3000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
+app.use(cors());
 
 const productRouter = require('./route');
 app.use('/api/v1/products', productRouter);
